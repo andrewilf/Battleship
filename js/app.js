09118b064e4a16eec1cd996aaca1379a3a51413f
andrewfaulkner <andrewfau1kn3r@gmail.com>
2021-09-17 17:59:54 +0800

    added ship naming object, and player class

--- js/app.js
+++ js/app.js
@@ -6,16 +6,112 @@ const Display = {
 
 }
 
+const shipNames = {
+    //pool of names which can be called for each ship
+    availableNames: [
+        'Czarevitch',
+        'Salsa',
+        'Gal-Pal',
+        'Pequest',
+        'Isidore-Cameleyre',
+        'Almkerk',
+        'Arnaldo',
+        'Shiromine Maru',
+        'BRAVADO',
+        'Aruba',
+        'Jib&Tonic',
+        'Amiral Sallandrouze de Lamornaix',
+        'Agios Georgios',
+        'Brighton',
+        'kartika',
+        'Murmanets',
+        'Pinebay',
+        'Whonos',
+        'Rover-1',
+        'Our Solitude',
+        'Speedlink Vanguard',
+        'Delpa II',
+        'Sea Skank',
+        'Ms Miriam',
+        'Dewy\'s bouey',
+        'Warmond',
+        'Almoural',
+        'Klarelfvan',
+        'Challeage',
+        'Arreton',
+        'Bornrif',
+        'Family Foto',
+        'Invor',
+        'Anna Sophia',
+        'Newhall Hills',
+        'Adler',
+        'Con-Tract-Oar',
+        'Bombard',
+        'Martin Van Buren',
+        'Pellegrini',
+        'Alder Park',
+        'Kwong Tung',
+        'Pyewacket',
+        'Itamar',
+        'Flow',
+        'ENCHANTRESS',
+        'Westmore',
+        'Myrmidon',
+        'Emily Rose',
+        'Leouardo Rodriguez',
+    ],
+    //ship names which are taken are added to this array
+    takenNames: [],
+    //method which choses a ship name, if not taken, add to taken names array and return the value. Otherwise try again
+    asignName: function () {
+        let done = false
+        while (!done) {
+            let choice = Math.ceil(Math.random() * this.availableNames.length - 1)
+            console.log(choice)
+            let currentName = this.availableNames[choice]
+            if (this.takenNames.find(name => name === currentName)) {
+                ``
+                console.log("name taken: ", currentName)
+            }
+            else {
+                console.log("found name")
+                this.takenNames.push(currentName)
+                done = true
+                console.log("name available: ", currentName)
+                return currentName
+            }
+        }
+    }
+}
+//common stats for each ship type
+const shipStats = {
+    carrier: {
+        length: 5,
+    },
+    battleship: {
+        length: 4,
+    },
+    cruiser: {
+        length: 3,
+    },
+    submarine: {
+        length: 3,
+    },
+    destroyer: {
+        length: 2,
+    },
+}
+
+// A cell object used to make a game board. Each cell has boolean states such as shotAt, occupied
 class Cell {
-    // A cell object used to make a game board. Each cell has boolean states such as shotAt, occupied
     constructor() {
         this.shotAt = false
         this.occupied = false
     }
 }
 
+// a board object consists of cell objects. Each board object belongs to one player
 class Board {
-    // a board object consists of cell objects. Each board object belongs to one player
     constructor(width, height) {
         this.board = []
         for (let i = 0; i < height; i++) {
@@ -29,10 +125,11 @@ class Board {
     }
 }
 
-class Battleship {
-    // generic Battleship object. may have children classes if more specific battleships are required
-    constructor(type, length, attack, health) {
+// generic Ship object. may have children classes if more specific battleships are required
+class Ship {
+    constructor(type, name, length, attack, health) {
         this.type = type
+        this.name = name
         //coordinates of the battleship. The four attributes are required to allow rotating the ship
         this.front = Math.ceil(length / 2)
         this.back = Math.floor(length / 2)
@@ -46,9 +143,9 @@ class Battleship {
         this.dead = false
 
     }
+    //returns true if ship is alive. If ship has 0 health, it returns false and the ship object this.dead is made true
     checkAlive() {
         if (this.health < 1) {
-            //returns true if ship is alive. If ship has 0 health, it returns false and the ship object this.dead is made true
             this.dead = true
             return false
         }
@@ -56,12 +153,73 @@ class Battleship {
     }
 }
 
+//player class
+class Player {
+    constructor(prefix, carriers, battleships, cruisers, submarines, destroyers) {
+        this.prefix = prefix
+        this.fleet = []
+        this.fleetCount = {
+            carrier: carriers,
+            battleship: battleships,
+            cruiser: cruisers,
+            submarine: submarines,
+            destroyer: destroyers
+        }
+    }
+    //checks the players fleet count and creates their fleet
+    createFleet() {
+        //loops for each ship type
+        for (const shipType in shipStats) {
+            for (let i = 0; i < this.fleetCount[shipType]; i++) {
+                //generates a ship name by combining the player prefix and the assigned name
+                const shipName = this.prefix + " " + shipNames.asignName()
+                //create ship and add to fleet                
+                const ship = new Ship(shipType, shipName, shipStats[shipType].length)
+                this.fleet.push(ship)
+            }
+        }
+    }
+    //adds a ship to fleet. More for debugging
+    addShip(type, name) {
+        const shipName = this.prefix + " " + name
+                //create ship and add to fleet                
+                const ship = new Ship(type, shipName, shipStats[type].length)
+                this.fleet.push(ship)
+    }
+    //calls the names of every ship currently in the players fleet
+    rollCall() {
+        for (const ship of this.fleet) {
+            console.log(`${ship.name} class ${ship.type}`)
+        }
+    }
+    //removes ship from fleet
+    shipDestroyed(destroyedShipName) {
+        const index = this.fleet.findIndex((ship) => ship.name === destroyedShipName)
+        if ( index !== -1) {
+            console.log("removing", this.fleet[index])
+            this.fleet.splice(index, 1, '')
+        }
+        else {
+            console.error("cannot remove a ship which does not exist")
+        }
+    }
+}
+
 const board = new Board(3, 2)
 console.log(board)
 
-const cruiser = new Battleship("cruiser", 5)
+const cruiser = new Ship("cruiser", "test", 5)
 console.log(cruiser)
 
+const player1 = new Player("U.S.S.", 1, 1, 1, 1, 1)
+player1.createFleet()
+console.log(player1)
+
+console.log(shipNames.asignName())
+console.log(shipNames.takenNames)
+player1.rollCall()
+player1.addShip("submarine", "test")
+player1.shipDestroyed("U.S.S. test")
 // $(() => {
 
 // })
